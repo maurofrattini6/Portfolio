8659211bb862c475d4a98b64f8900ddf702923f3
Maurizio Frattini <mauro_medellin@hotmail.com>
2019-02-21 12:23:59 -0500

    Java Blog

---
+++ java/Blog Project Java/src/main/java/com/tts/blogproject/BlogPosts/BlogPost.java
@@ -0,0 +1,69 @@
+package com.tts.blogproject.BlogPosts;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BlogPost {
+    //sets Id as primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    private String blogEntry;
+
+    public BlogPost() {
+        //non argument constructor needed for JPA
+    }
+
+    public BlogPost(String title, String author, String blogEntry) {
+        this.title = title;
+        this.author = author;
+        this.blogEntry = blogEntry;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBlogEntry() {
+        return blogEntry;
+    }
+
+    public void setBlogEntry(String blogEntry) {
+        this.blogEntry = blogEntry;
+    }
+
+    @Override
+    public String toString() {
+        return "BlogPosts{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", blogEntry='" + blogEntry + '\'' +
+                '}';
+    }
+}
